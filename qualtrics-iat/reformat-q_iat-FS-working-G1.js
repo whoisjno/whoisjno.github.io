045ff1a2636aedd83b9d98551c744b6e522aba23
JNo <jnoelgeorge@gmail.com>
2025-02-28 10:00:49 +1100

    moved files and create G1 and G2 scripts

---
+++ qualtrics-iat/reformat-q_iat-FS-working-G1.js
@@ -0,0 +1,48 @@
+define(['pipAPI', 'https://whoisjno.github.io/qualtrics-iat/quiat9.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+		category1 : {
+			name : 'A-Robots', //Will appear in the data.
+			title : {
+				media : {word : 'Robot'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'zenbo.jpg'},
+    			{image : 'mero.jpg'},
+    			{image : 'int-ball.jpg'},
+    			{image : 'speech buddy.jpg'},
+    			{image : 'maggie.jpg'},
+    			{image : 'papero.jpg'},
+    			{image : 'irobi-q.jpg'}
+			],
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em','height':'350px !important','width':'auto !important'}
+		},
+		category2 :	{
+			name : 'Toys', //Will appear in the data.
+			title : {
+				media : {word : 'Toy'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : '2005.jpg'}, //LF
+					{image : '2001.jpg'}, //HF
+    			{image : '2021.jpg'}, //LF
+    			{image : '2023.jpg'}, //HF
+    			{image : '2025.jpg'}, //LF
+    			{image : '2035.jpg'}, //HF
+    			{image : '2048.jpg'}			], //HF
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em','height':'350px !important','width':'auto !important'}
+		},
+
+		base_url : {//Where are your images at?
+			image : 'https://whoisjno.github.io/qualtrics-iat/images/'
+		},
+		fullscreen:true
+	});
+});
---
+++ qualtrics-iat/reformat-q_iat-FS-working-G2.js
@@ -0,0 +1,48 @@
+define(['pipAPI', 'https://whoisjno.github.io/qualtrics-iat/quiat9.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+		category1 : {
+			name : 'M-Robots', //Will appear in the data.
+			title : {
+				media : {word : 'Robot'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'ethon2.jpg'},
+    			{image : 'franka emika.jpg'},
+    			{image : 'homemate.jpg'},
+    			{image : 'jibo.jpg'},
+    			{image : 'mykie.jpg'},
+    			{image : 'telubee.jpg'},
+    			{image : 'tjbot.jpg'}
+			],
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em','height':'500px','width':'500px'}
+		},
+		category2 :	{
+			name : 'Toys', //Will appear in the data.
+			title : {
+				media : {word : 'Toy'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : '2018.jpg'}, //HF
+    			{image : '2029.jpg'}, //HF
+    			{image : '2033.jpg'}, //LF
+    			{image : '2044.jpg'}, //LF
+    			{image : '2049.jpg'}, //HF
+    			{image : '2055.jpg'}, //HF
+    			{image : '2056.jpg'}			], //HF
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em','height':'500px','width':'500px'}
+		},
+
+		base_url : {//Where are your images at?
+			image : 'https://whoisjno.github.io/qualtrics-iat/images/'
+		},
+		fullscreen:true
+	});
+});
