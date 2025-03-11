81f19205a117adb87ed1b99b339dea2545e7451b
JNo <jnoelgeorge@gmail.com>
2025-03-12 09:19:40 +1100

    edit origin biat to see if i can do multi categories

--- qualtrics-biat/quabiat_v2.js
+++ qualtrics-biat/quabiat_v2.js
@@ -1,15 +1,53 @@
-define(['pipAPI', 'https://whoisjno.github.io/qualtrics-biat/sub-biat.js'], function(APIConstructor, batExtension){
+define(['pipAPI', 'https://whoisjno.github.io/qualtrics-biat/biat6.js'], function(APIConstructor, iatExtension){
   var API = new APIConstructor();
 
-	return batExtension({
-		canvas : {
-			maxWidth: 725,
-			proportions : 0.85,
-			background: '#ffffff',
-			borderWidth: 5,
-			canvasBackground: '#ffffff',
-			borderColor: 'lightblue'
-		},
+
+	return iatExtension({
+		practiceCategory1 :
+			{
+				name : 'Mammals', //Will appear in the data.
+				title : {
+					media : {word : 'Mammals'}, //Name of the category presented in the task.
+					css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
+					height : 4, //Height (because we need to know where to put the next item in the title)
+					startStimulus : {
+					//If you're using a startStimulus, set here. If not, set the parameter showStimuliWithInst to false (see later below)
+						media : {word : 'Dogs, Horses, Cows, Lions'},
+						css : {color:'#31b404','font-size':'1em'},
+						height : 2
+					}
+				},
+				stimulusMedia : [ //Stimuli content as PIP's media objects
+					{word : 'Dogs'},
+					{word : 'Horses'},
+					{word : 'Lions'},
+					{word : 'Cows'}
+				],
+				//Stimulus css (style of the stimuli)
+				stimulusCss : {color:'#31b404','font-size':'2em'}
+			},
+			practiceCategory2 :
+			{
+				name : 'Birds',
+				title : {
+					media : {word : 'Birds'},
+					css : {color:'#31b404','font-size':'1.8em'},
+					height : 4,
+					startStimulus : {
+						media : {word : 'Pigeons, Swans, Crows, Ravens'},
+						css : {color:'#31b404','font-size':'1em'},
+						height : 2
+					}
+				},
+				stimulusMedia : [ //Stimuli content as PIP's media objects
+					{word : 'Pigeons'},
+					{word : 'Swans'},
+					{word : 'Crows'},
+					{word : 'Ravens'}
+				],
+				//Stimulus css
+				stimulusCss : {color:'#31b404','font-size':'2em'}
+			},
 		categories : [
 			{
 				name : 'aRobots',
@@ -141,27 +179,9 @@ define(['pipAPI', 'https://whoisjno.github.io/qualtrics-biat/sub-biat.js'], func
 				image : 'https://whoisjno.github.io/qualtrics-biat/images/'
 			},
 			showStimuliWithInst : false,
-			fullscreen : true,
-
-blockSequenceID : Math.round(Math.random() * 5)
-
-
+			fullscreen : true
 	});
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
