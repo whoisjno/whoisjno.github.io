9df5f9ade4f0f47d8109b20cddd1b2c7e1f0e19b
JNo <jnoelgeorge@gmail.com>
2025-03-12 16:39:33 +1100

    added an allstim amendment to record other blocks

--- qualtrics-iat/quiat_fourStim-5b.js
+++ qualtrics-iat/quiat_fourStim-5b.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://whoisjno.github.io/qualtrics-iat/quiat10.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://whoisjno.github.io/qualtrics-iat/quiat10-allBlocks.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 		return iatExtension({
