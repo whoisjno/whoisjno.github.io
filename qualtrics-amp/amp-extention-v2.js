da01d23e1e5a6b133dc93f319b536933301bf975
JNo <jnoelgeorge@gmail.com>
2026-02-04 15:27:56 +1100

    change link to sourcecode to self-hosted

--- qualtrics-amp/amp-extention-v2.js
+++ qualtrics-amp/amp-extention-v2.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualtrics/qamp.js'], function(APIConstructor, ampExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/whoisjno/whoisjno.github.io@ece982e57bdf9720375ef34f7186aaa902ba9ca9/qualtrics-amp/amp-sourcecode.js'], function(APIConstructor, ampExtension){
 
 	var API = new APIConstructor();
 	
